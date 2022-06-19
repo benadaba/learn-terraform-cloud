7246f623257bb48f79832cf44b77323ec0e60c60
benadaba <adabankah.com@gmail.com>
2022-06-19 03:48:06 +0100

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "datapandas"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
